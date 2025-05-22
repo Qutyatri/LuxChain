cb5619c675517c8ebc088b710dd08293475267a6
Yatri Patel <n11676302@qut.edu.au>
2025-05-22 20:21:11 +1000

    Added the code for all three smart contracts in one file to make it work together.

--- code/Combinedcontracts.sol
+++ code/Combinedcontracts.sol
@@ -1,88 +1,103 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.24;
 
-contract MintingContract {
-    uint public tokenCount = 0;
+contract OwnershipContract {
+    mapping(uint256 => address) public nftOwners;
+
+    function transferOwnership(uint256 tokenId, address newOwner) external {
+        require(msg.sender == nftOwners[tokenId], "Only owner can transfer");
+        nftOwners[tokenId] = newOwner;
+    }
+
+    function getOwner(uint256 tokenId) external view returns (address) {
+        return nftOwners[tokenId];
+    }
+
+    // Called by MintingContract to set the manufacturer as the initial owner
+    function setInitialOwner(uint256 tokenId, address owner) external {
+        require(nftOwners[tokenId] == address(0), "Already assigned");
+        nftOwners[tokenId] = owner;
+    }
+}
+
+contract VerificationContract {
     address public admin;
+    mapping(address => bool) public authenticators;
+    mapping(uint256 => bool) public verified;
+
+    modifier onlyAuthenticator() {
+        require(authenticators[msg.sender], "Not an authenticator");
+        _;
+    }
 
     constructor() {
         admin = msg.sender;
     }
 
-    struct ProductNFT {
-        uint tokenId;
+    function approveAuthenticator(address _authenticator) external {
+        require(msg.sender == admin, "Only admin");
+        authenticators[_authenticator] = true;
+    }
+
+    function verifyProduct(uint256 tokenId) external onlyAuthenticator {
+        verified[tokenId] = true;
+    }
+
+    function isVerified(uint256 tokenId) external view returns (bool) {
+        return verified[tokenId];
+    }
+}
+
+contract MintingContract {
+    address public admin;
+    OwnershipContract public ownershipContract;
+    VerificationContract public verificationContract;
+
+    uint256 public nextTokenId;
+
+    struct Product {
         string model;
         string serialNumber;
-        address currentOwner;
-        address[] previousOwners;
-        bool isVerified;
+        address manufacturer;
     }
 
-    mapping(uint => ProductNFT) public nfts;
-    mapping(address => bool) public isVerifier;
-
-    event Minted(uint tokenId, string model, string serialNumber, address owner);
-    event OwnershipTransferred(uint tokenId, address from, address to);
-    event VerifierApproved(address verifier);
-    event ProductVerified(uint tokenId, address verifier);
+    mapping(uint256 => Product) public products;
+    mapping(address => bool) public manufacturers;
 
     modifier onlyAdmin() {
-        require(msg.sender == admin, "Only admin can do this");
+        require(msg.sender == admin, "Only admin");
         _;
     }
 
-    modifier onlyVerifier() {
-        require(isVerifier[msg.sender], "Only approved verifiers can verify");
+    modifier onlyManufacturer() {
+        require(manufacturers[msg.sender], "Only manufacturer");
         _;
     }
 
-    // Mint product NFT
-    function mint(string memory _model, string memory _serialNumber) public {
-        tokenCount++;
-
-        ProductNFT storage newNFT = nfts[tokenCount];
-        newNFT.tokenId = tokenCount;
-        newNFT.model = _model;
-        newNFT.serialNumber = _serialNumber;
-        newNFT.currentOwner = msg.sender;
-
-        emit Minted(tokenCount, _model, _serialNumber, msg.sender);
+    constructor(address _ownershipAddr, address _verificationAddr) {
+        admin = msg.sender;
+        ownershipContract = OwnershipContract(_ownershipAddr);
+        verificationContract = VerificationContract(_verificationAddr);
     }
 
-    // Transfer ownership
-    function transferOwnership(uint tokenId, address newOwner) public {
-        require(nfts[tokenId].currentOwner == msg.sender, "Only current owner can transfer");
-
-        nfts[tokenId].previousOwners.push(msg.sender);
-        nfts[tokenId].currentOwner = newOwner;
-
-        emit OwnershipTransferred(tokenId, msg.sender, newOwner);
+    function approveManufacturer(address manufacturer) external onlyAdmin {
+        manufacturers[manufacturer] = true;
     }
 
-    // Approve an address as verifier
-    function approveVerifier(address _verifier) public onlyAdmin {
-        isVerifier[_verifier] = true;
-        emit VerifierApproved(_verifier);
+    function mintNFT(string calldata model, string calldata serialNumber) external onlyManufacturer {
+        uint256 tokenId = nextTokenId++;
+        products[tokenId] = Product(model, serialNumber, msg.sender);
+        ownershipContract.setInitialOwner(tokenId, msg.sender);
     }
 
-    // Verifier confirms product authenticity
-    function verifyProduct(uint tokenId) public onlyVerifier {
-        nfts[tokenId].isVerified = true;
-        emit ProductVerified(tokenId, msg.sender);
+    function getProductDetails(uint256 tokenId) public view returns (
+        string memory model,
+        string memory serialNumber,
+        address manufacturer
+    ) {
+        Product memory p = products[tokenId];
+        return (p.model, p.serialNumber, p.manufacturer);
     }
 
-    // Get verification status
-    function checkVerification(uint tokenId) public view returns (bool) {
-        return nfts[tokenId].isVerified;
-    }
-
-    // Get current owner
-    function getCurrentOwner(uint tokenId) public view returns (address) {
-        return nfts[tokenId].currentOwner;
-    }
-
-    // Get ownership history
-    function getPreviousOwners(uint tokenId) public view returns (address[] memory) {
-        return nfts[tokenId].previousOwners;
-    }
+    
 }
